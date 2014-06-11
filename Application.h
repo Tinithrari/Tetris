1db8eee287455a01b6103b1e0ab82b86cfd37304
Tinithrari <xavier.heugue@gmail.com>
2014-06-11 16:58:19 +0200

    Envois de fichier

---
+++ Application.h
@@ -0,0 +1,33 @@
+#ifdef SFML_STATIC
+#pragma comment(lib, "glew.lib")
+#pragma comment(lib, "freetype.lib")
+#pragma comment(lib, "jpeg.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "gdi32.lib")  
+
+#endif // SFML_STATIC
+
+#include <SFML/Graphics.hpp>
+#include <cassert>
+#include <iostream>
+#include "Scene.h"
+
+using namespace std;
+
+class Application {
+
+private:
+	// Graphique
+	sf::RenderWindow		_window;
+	
+	//Logique
+	Scene					_scene;
+
+public:
+	Application();
+	void run();
+	void processEvent();
+	void update();
+	void render();
+};
