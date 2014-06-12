c005ec394ba1d14b55db2be802845c06ea1f56b9
Tinithrari <xavier.heugue@gmail.com>
2014-06-12 21:32:29 +0200

    Push

--- Tetris/Grille.cpp
+++ Tetris/Grille.cpp
@@ -152,7 +152,7 @@ void Grille::handleEvent()
 			_etat = ACTIVATE;
 		}
 		/*
-			Ajout de la pause ici (utiliser un else if)glaviersqddq
+			Ajout de la pause ici (utiliser un else if)
 		*/
 		else
 		{
--- Tetris/Grille.h
+++ Tetris/Grille.h
@@ -14,7 +14,7 @@ class Grille{
 private:
 	// Graphique
 	sf::RectangleShape		_grille;
-	tetramino				grille;//iii
+	tetramino				grille;
 	Tetramino				_tetramino;
 	Tetramino				_suivant;
 	// Logique
